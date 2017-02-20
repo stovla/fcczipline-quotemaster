c18197d90bb7e1cf928e154a44d966aff500c9df
Vlastimir <vlastimirsd@gmail.com>
2017-02-20 12:15:36 +0000

    added quote app to git repo

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/* using jquery method */
+
+$(document).ready(init);
+
+function init() {
+	// body...
+    getQuote();
+	$('#new-quote').on("click", getQuote);
+}
+
+function getQuote () {
+	var text;
+	var author;
+  /* getting the api file and putting it in the 'json' atribute */
+    $.getJSON("http://api.forismatic.com/api/1.0/?format=jsonp&method=getQuote&jsonp=?&lang=en", function(json) {
+		text = json.quoteText;
+		 author = json.quoteAuthor;
+        
+		$('#qoute-text').text('"' + text + '"');
+
+		 if (author == "") {
+		 	$('#quote-author').text('- No author -');
+		 } else {
+			 $('#quote-author').text('- ' + author + ' -');
+		 }
+         $('#tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text=' + text).attr('target', '_blank');
+
+    });
+}
